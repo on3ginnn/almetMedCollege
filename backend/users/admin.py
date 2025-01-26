5f8c532e6901aa6ae119132c7a6e1540fb9da2c6
on3gin <on3ginnn@gmail.com>
2025-01-26 23:22:53 +0300

    f

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+import users.models
 
-# Register your models here.
+
+@admin.register(users.models.Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'course')
+    list_display_links = ('name', )
+    list_filter = ('course',)
+    search_fields = ('name',)
+    ordering = ('course', 'name')
+    fields = ('name', 'course')
+
+    def has_add_permission(self, request):
+        return request.user.is_staff
+
+
+@admin.display(description="Полное имя")
+def user_full_name(obj):
+    return f"{obj.first_name} {obj.last_name}".capitalize()
+
+
+@admin.register(users.models.User)
+class CustomUserAdmin(UserAdmin):
+    site_header = "Панель администрирования"
+    # print(super().get_fieldsets(request))
+    # print(get_fieldsets())
+
+    fieldsets = (
+        ('Учетные данные', {'fields': ('username', 'password')}), 
+        ('Персональные данные', {'fields': ('first_name', 'last_name', 'email', 'group')}), 
+        ('Роль', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    print(fieldsets)
+    list_display = ('id', user_full_name, 'group', 'is_staff', 'is_active')
+    list_display_links = (user_full_name, )
+    list_filter = ('groups', 'is_staff', 'is_active', 'group')
+    search_fields = ('username', 'email')
+    ordering = ('last_name', 'first_name', 'id')
+
+    autocomplete_fields = ['groups']
+    search_fields = ['last_name', "first_name"]
+
+    # Убрать возможность регистрации самим пользователям
+    def has_add_permission(self, request):
+        return request.user.is_staff
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,48 @@
-from django.db import models
+import django.db.models
 import django.contrib.auth.models
+import django.core.exceptions
 
+
+class Group(django.db.models.Model):
+    COURSE_CHOICES = [
+        (1, "1 курс"),
+        (2, "2 курс"),
+        (3, "3 курс"),
+        (4, "4 курс"),
+    ]
+
+    name = django.db.models.CharField(max_length=5, verbose_name="Название группы")
+    course = django.db.models.IntegerField(choices=COURSE_CHOICES, verbose_name="Номер курса")
+
+    class Meta:
+        unique_together = ('name', 'course')  # Уникальность пары "название-курс"
+
+    def clean(self):
+        if self.course is None:
+            raise django.core.exceptions.ValidationError("Номер курса обязателен для заполнения.")
+        if self.course < 1 or self.course > 4:
+            raise django.core.exceptions.ValidationError("Курс должен быть от 1 до 4")
+
+    def __str__(self):
+        return f"{self.name} ({self.get_course_display()})"
+    
 
 class User(django.contrib.auth.models.AbstractUser):
-    pass
+    # class Role(django.db.models.TextChoices):
+    #     STUDENT = 'student', 'Student'
+    #     TEACHER = 'teacher', 'Teacher'
+    #     ADMIN = 'admin', 'Administrator'
+
+    # role = django.db.models.CharField(
+    #     max_length=10,
+    #     choices=Role.choices,
+    #     default=Role.STUDENT,
+    # )
+
+    group = django.db.models.ForeignKey(
+        'Group',
+        null=True, 
+        blank=True, 
+        on_delete=django.db.models.SET_NULL,
+        related_name='group_students',
+    )  # Только для студентов
