a31e839bf129027040d5dfcb36301092d9eadd1c
on3ginnn <124593750+on3ginnn@users.noreply.github.com>
2025-02-06 12:56:31 +0300

    search news by title

--- backend/news/urls.py
+++ backend/news/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create/', news.views.NewsCreateAPIView.as_view(), name='create'),
     path('all/', news.views.NewsListAPIView.as_view(), name='list'),
     path('<int:pk>/', news.views.NewsDetailUpdateDeleteAPIView.as_view(), name='concrete'),
+    path('', news.views.SearchNewsAPIView.as_view(), name='search'),
 ]
--- backend/news/views.py
+++ backend/news/views.py
@@ -1,5 +1,6 @@
 import rest_framework.generics
 import rest_framework.permissions
+import rest_framework.response
 
 import news.serializer
 import users.permissions
@@ -22,3 +23,16 @@ class NewsDetailUpdateDeleteAPIView(rest_framework.generics.RetrieveUpdateDestro
     permission_classes = [users.permissions.DjangoModelPermissionsWithGroups]
     queryset = news.models.News.objects.all()
     serializer_class = news.serializer.NewsCreateUpdateSerializer
+
+
+class SearchNewsAPIView(rest_framework.views.APIView):
+    def get(self, request):
+        search_query = request.GET.get('search')
+        # TODO: поиск должен быть не чувствительным к регистру
+        if search_query:
+            news_list = news.models.News.objects.filter(title__icontains=search_query)
+            serializer = news.serializer.NewsSerializer(news_list, many=True)
+            
+            return rest_framework.response.Response(serializer.data)
+        
+        return rest_framework.response.Response([])
