3c968035d52eaac4e4fc517065dfbf63c1a2c49d
on3ginnn <124593750+on3ginnn@users.noreply.github.com>
2025-02-06 12:17:07 +0300

    add new app conf urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,8 @@ import django.urls
 import backend.views
 
 import users.urls
+import group.urls
+import news.urls
 
 
 urlpatterns = [
@@ -12,5 +14,7 @@ urlpatterns = [
     #     name="ajax-autocomplete",
     # ),
     django.urls.path("user/", django.urls.include(users.urls)),
+    django.urls.path("group/", django.urls.include(group.urls)),
+    django.urls.path("news/", django.urls.include(news.urls)),
     django.urls.path("admin/", django.contrib.admin.site.urls),
 ]
