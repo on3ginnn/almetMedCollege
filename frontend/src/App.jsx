edcda5670181b6b92643224ae2cfbb4c69bd20cf
on3ginnn <124593750+on3ginnn@users.noreply.github.com>
2025-02-19 12:59:46 +0300

    add mui front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MainContent from './mainContent';
+// import Main from './components/main';
+import { BrowserRouter } from 'react-router-dom';
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <MainContent />
+
+      </Box>
+    </Container>
+    // <BrowserRouter>
+    // <Header/>
+    // <Main/>
+    // </BrowserRouter>
+
   )
+    
 }
 
 export default App
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,20 @@
+// import { Button, Nav, Navbar, Container, Offcanvas, Form } from "react-bootstrap"
+// import { Link, useNavigate } from "react-router-dom";
+// import { authRoutes, publicRoutes } from "../config/routes";
+// import { observer } from "mobx-react";
+// import { userStore } from "../stores/UserStore";
+// import "../assets/css/header.css";
+// import { motion } from 'framer-motion';
+
+const Header = () => {
+    const brandName = "KIBET";
+    const expand = "md";
+    // const logout = ()=>{
+    //     userStore.logout();
+    //         navigate('/login');
+    // }
+    return (
+        <h1>шапка</h1>
+    )
+}
+export default Header;
