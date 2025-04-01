2b5e1702e189a45a4f5bc6d7bd67f2c0d6d83f92
on3ginnn <124593750+on3ginnn@users.noreply.github.com>
2025-04-01 15:12:56 +0300

    Add files via upload

--- frontend/src/api/scheduleAPI.js
+++ frontend/src/api/scheduleAPI.js
@@ -1,5 +1,12 @@
 import { apiClient } from "./../config/APIClient";
 
 export default class ScheduleAPI {
-
+    static async getSchedule(date, group) {
+        try {
+            const response = await apiClient.get(`/schedule/`, {params: {date: date, group: group}});
+            return response;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
--- frontend/src/stores/scheduleStore.js
+++ frontend/src/stores/scheduleStore.js
@@ -3,5 +3,15 @@ import ScheduleAPI from '../api/scheduleAPI';
 
 
 export const scheduleStore = create((set) => ({
-
+    schedule: null,
+    getSchedule: async (date, group) => {
+        try {
+            const response = await ScheduleAPI.getSchedule(date, group);
+            console.warn(`get schedule with code ${response.status}`);
+            set({ schedule: response.data});
+        } catch (error) {
+            console.error('Ошибка при получении расписания:', error);
+            throw error; // Перебрасываем ошибку для обработки выше
+        }
+    },
 }));
